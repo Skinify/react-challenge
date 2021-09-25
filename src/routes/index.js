e22e8a1876648ede19cd3f00239e0f07d2c1abc3
skinify <gabrielZK2000@hotmail.com>
2021-09-25 17:14:49 -0300

    Redirecting user only to existing pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,18 +2,21 @@ import React from "react";
 import Main from "../pages/Main";
 import Level from "../pages/Level";
 import Questions from "../pages/Questions";
-import { Switch, Route, useLocation } from "react-router-dom";
+import { Switch, Route, useLocation, useHistory } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 
 const App = () => {
   const location = useLocation();
+  const history = useHistory();
 
   return (
       <AnimatePresence exitBeforeEnter>
          <Switch location={location} key={location.pathname}>
             <Route path="/" exact component={Main} />
             <Route path="/level" exact component={Level} />
+            <Route path="/questions/" exact render={() =>history.push('/level')}/>
             <Route path="/questions/:questionsNumber" exact component={Questions} />
+            <Route path='*' exact render={() =>history.push('/')}/>
         </Switch>
     </AnimatePresence>
   );
